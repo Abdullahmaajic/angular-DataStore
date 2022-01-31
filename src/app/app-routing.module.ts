16313b74cff49b1b0dcaf60491d96f7f0984d724
Abdullahmaajic <abdullahmaajic@gmail.com>
2022-01-31 16:31:50 +0530

    updated-data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { DeleteproductComponent } from './deleteproduct/deleteproduct.component';
+import { GetproductComponent } from './getproduct/getproduct.component';
+import { UpdateproductComponent } from './updateproduct/updateproduct.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'add',
+    component:AddproductComponent
+  },
+  {
+    path:'delete',
+    component:DeleteproductComponent
+  },
+  {
+    path:'update',
+    component:UpdateproductComponent
+  },
+  {
+    path:'get',
+    component:GetproductComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { GetproductComponent } from './getproduct/getproduct.component';
+import { DeleteproductComponent } from './deleteproduct/deleteproduct.component';
+import { UpdateproductComponent } from './updateproduct/updateproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproductComponent,
+    GetproductComponent,
+    DeleteproductComponent,
+    UpdateproductComponent
   ],
   imports: [
     BrowserModule,
